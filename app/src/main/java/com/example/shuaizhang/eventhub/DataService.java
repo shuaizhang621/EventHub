eb8719f948e57137fa082059a8c1ad6b41b82f56
shuaizhang621 <sz1950@nyu.edu>
2018-03-24 10:52:41 -0400

    First commit

---
+++ app/src/main/java/com/example/shuaizhang/eventhub/DataService.java
@@ -0,0 +1,25 @@
+package com.example.shuaizhang.eventhub;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by shuaizhang on 3/4/18.
+ */
+
+public class DataService {
+    /**
+     * Fake all the event data for now. We will refine this and connect
+     * to our backend later.
+     */
+    public static List<Event> getEventData() {
+        List<Event> eventData = new ArrayList<Event>();
+        for (int i = 0; i < 10; ++i) {
+            eventData.add(
+                    new Event("Event", "1184 W valley Blvd, CA 90101",
+                            "This is a huge event"));
+        }
+        return eventData;
+    }
+
+}

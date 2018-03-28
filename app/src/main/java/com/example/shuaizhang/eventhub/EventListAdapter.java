debedbd6d627b15a4882faaaa327bf095df349d5
shuaizhang621 <sz1950@nyu.edu>
2018-03-27 22:38:59 -0400

    like event feature added

--- app/src/main/java/com/example/shuaizhang/eventhub/EventListAdapter.java
+++ app/src/main/java/com/example/shuaizhang/eventhub/EventListAdapter.java
@@ -9,6 +9,12 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.List;
 
 /**
@@ -18,6 +24,8 @@ import java.util.List;
 
 public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.ViewHolder> {
     private List<Event> eventList;
+    private DatabaseReference databaseReference;
+
 
     /**
      * Constructor for EventListAdapter
@@ -26,6 +34,7 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.View
      */
     public EventListAdapter(List<Event> events) {
         eventList = events;
+        databaseReference = FirebaseDatabase.getInstance().getReference();
     }
 
     /**
@@ -41,6 +50,13 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.View
         public ImageView imgview;
         public View layout;
 
+        public ImageView img_view_good;
+        public ImageView img_view_comment;
+
+        public TextView good_number;
+        public TextView comment_number;
+
+
         public ViewHolder(View v) {
             super(v);
             layout = v;
@@ -49,6 +65,12 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.View
             description = (TextView) v.findViewById(R.id.event_item_description);
             time = (TextView) v.findViewById(R.id.event_item_time);
             imgview = (ImageView) v.findViewById(R.id.event_item_img);
+
+            img_view_good = (ImageView) v.findViewById(R.id.event_good_img);
+            img_view_comment = (ImageView) v.findViewById(R.id.event_comment_img);
+            good_number = (TextView) v.findViewById(R.id.event_good_number);
+            comment_number = (TextView) v.findViewById(R.id.event_comment_number);
+
         }
     }
 
@@ -65,6 +87,8 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.View
         holder.location.setText(locations[1] + "," + locations[2]);
         holder.description.setText(event.getDescription());
         holder.time.setText(Utils.timeTransformer(event.getTime()));
+
+        holder.good_number.setText(String.valueOf(event.getLike()));
 
         if (event.getImgUri() != null) {
             final String url = event.getImgUri();
@@ -83,6 +107,33 @@ public class EventListAdapter extends RecyclerView.Adapter<EventListAdapter.View
         } else {
             holder.imgview.setVisibility(View.GONE);
         }
+
+        //When user likes the event, push like number to firebase database
+        holder.img_view_good.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                databaseReference.child("events").addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                            Event recordedevent = snapshot.getValue(Event.class);
+                            if (recordedevent.getId().equals(event.getId())) {
+                                int number = recordedevent.getLike();
+                                holder.good_number.setText(String.valueOf(number + 1));
+                                snapshot.getRef().child("like").setValue(number + 1);
+                                break;
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
 
     }
 

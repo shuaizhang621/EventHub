83a37bab0338200e34cd40058544683ee0bc8d71
shuaizhang621 <sz1950@nyu.edu>
2018-04-01 21:26:51 -0400

    map

--- app/src/main/java/com/example/shuaizhang/eventhub/Event.java
+++ app/src/main/java/com/example/shuaizhang/eventhub/Event.java
@@ -16,6 +16,8 @@ public class Event {
     private long time;
     private String username;
     private String imgUri;
+    private double latitude;
+    private double longitude;
 
     private int CommentNumber;
 
@@ -81,6 +83,23 @@ public class Event {
     public void setCommentNumber(int commentNumber) {
         CommentNumber = commentNumber;
     }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
 
     /**
      * Constructor
--- app/src/main/java/com/example/shuaizhang/eventhub/EventActivity.java
+++ app/src/main/java/com/example/shuaizhang/eventhub/EventActivity.java
@@ -1,12 +1,16 @@
 package com.example.shuaizhang.eventhub;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class EventActivity extends AppCompatActivity {
 
     private Fragment mEventsFragment;
+    private Fragment mEventMapFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +20,39 @@ public class EventActivity extends AppCompatActivity {
         if (mEventsFragment == null) {
             mEventsFragment = new EventsFragment();
         }
+
+        //mEventMapFragment = new EventMapFragment();
+
         getSupportFragmentManager().beginTransaction().
                 add(R.id.relativelayout_event, mEventsFragment).commit();
+
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.navigation);
+
+        // Set Item click listener to the menu items
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.action_event_list:
+                                getSupportFragmentManager().beginTransaction()
+                                        .replace(R.id.relativelayout_event,
+                                                mEventsFragment).commit();
+                                break;
+                            case R.id.action_event_map:
+                                if (mEventMapFragment == null) {
+                                    mEventMapFragment = new EventMapFragment();
+                                }
+                                getSupportFragmentManager().beginTransaction()
+                                        .replace(R.id.relativelayout_event,
+                                                mEventMapFragment).commit();
+
+                        }
+                        return false;
+                    }
+                });
+
 
     }
 }
